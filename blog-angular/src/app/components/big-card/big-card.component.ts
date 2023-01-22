1c6ee8c99078aa830cbfbb92c9d8732e19c4df27
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2023-01-22 17:26:51 -0300

    leaving dynamic content

--- blog-angular/src/app/components/big-card/big-card.component.ts
+++ blog-angular/src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  imageNews: string = '';
+  @Input()
+  titleNews: string = '';
+  @Input()
+  subtitleNews: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
